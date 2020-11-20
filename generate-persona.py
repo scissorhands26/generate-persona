a91f56dbc36f2ec833f8b26f630959fd8dcef54e
Ryan Scissons <r.scissons@hotmail.com>
2020-11-19 21:56:01 -0500

    Update to using classes

--- generate-persona.py
+++ generate-persona.py
@@ -1,38 +1,42 @@
 #!/usr/bin/env python3
 import random,string,names
 
-def createPersona():
+class alias:
 
-	# name and email alias generation
-	firstName = names.get_first_name()
-	lastName = names.get_last_name()
-	name = firstName + " " + lastName
-	emailName = firstName + '.' + lastName
-	emailRandomization = random.randint(1000000,99999999)
-	email = emailName + '.' + str(emailRandomization) + '@protonmail.com'
+	def __init__(self, fname, lname):
+		self.fname = fname
+		self.lname = lname
+		self.name = fname + ' ' + lname
+		self.email = fname + '.' + lname + '@protonmail.com'
+		self.password = createPassword()
 
-	# password generation
+def createPassword():
 	password = []
 	characters = string.ascii_letters + string.digits + string.punctuation
 	passwordlength = 24
 	for x in range (passwordlength):
 		password.append(random.choice(characters))
 	password = (''.join(password))
+	return password
 
-	# prepare to write
-	profile = name + '\n' + password + '\n' + email + '\n'
-	return profile
-
-def createProfile(profile):
+def createProfile():
 	
-	# write to file
+	fname = names.get_first_name()
+	lname = names.get_last_name()
+	
+	profile = alias(fname, lname)
+
+	person = profile.name + '\n' + profile.email + '\n' + profile.password + '\n'
+	return person
+
+def createPersona(person):
+
 	with open("persona", "w") as file:
-		data = file.write(createPersona())
+		data = file.write(createProfile())
 		print(data)
 
 def main():
-	createPersona()
-	createProfile(createPersona)
+	createPersona(createProfile())
 
 if __name__ == "__main__":
-    main()
+	main()
